20af052058daf76c85cfc3d69197a4c13c64d091
paul <paulmperez25@gmail.com>
2025-12-11 17:17:20 -0400

    agregando github action

--- src/test.py
+++ src/test.py
@@ -1,11 +1,13 @@
 from app import Calculadora
 def test_sums_2_numeros();
     assert, Calculadora().sum(2,2) ==4
+    #instalacion y sus paquetes
     #pip install pytest
     #pytest .\src\test.py
     #git init
     #git add .
     #git commit -m "inicio"
     #git branch -M main
+    #git branch
     #git remote add origin  .....
     #git push -u origin main
