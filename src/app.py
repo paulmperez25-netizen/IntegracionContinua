6fc03a393f98f2fe5902501f70d28ece248aadcf
paul <paulmperez25@gmail.com>
2025-12-11 16:13:07 -0400

    inicio

---
+++ src/app.py
@@ -0,0 +1,4 @@
+import os 
+class Calculadora:
+    def sum(self,a:int,b:int) -> init;
+        return 0 #a + b
---
+++ src/test.py
@@ -0,0 +1,11 @@
+from app import Calculadora
+def test_sums_2_numeros();
+    assert, Calculadora().sum(2,2) ==4
+    #pip install pytest
+    #pytest .\src\test.py
+    #git init
+    #git add .
+    #git commit -m "inicio"
+    #git branch -M main
+    #git remote add origin  .....
+    #git push -u origin main
