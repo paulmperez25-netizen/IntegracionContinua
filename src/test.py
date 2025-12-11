66b2d7b250a54b44a324c74a01588a11cf7bb81f
paul <paulmperez25@gmail.com>
2025-12-11 17:26:36 -0400

    agregando github action12

--- src/test.py
+++ src/test.py
@@ -1,6 +1,10 @@
 from app import Calculadora
 def test_sums_2_numeros();
     assert, Calculadora().sum(2,2) ==4
+
+
+
+    
     #instalacion y sus paquetes
     #pip install pytest
     #pytest .\src\test.py
